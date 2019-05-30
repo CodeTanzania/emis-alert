77328ee905041cc8e08e7ad4550c1d479f807480
lykmapipo <lallyelias87@gmail.com>
2019-05-30 17:25:54 +0300

    feat: format alert dates on exports and reorder columns

--- test/bootstrap.spec.js
+++ test/bootstrap.spec.js
@@ -6,13 +6,4 @@ process.env.NODE_ENV = 'test';
 process.env.DEFAULT_LOCALE = 'en';
 process.env.DEBUG = true;
 
-
-/* setup mongoose */
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-
-/* setup */
-require('chai').use(require('sinon-chai'));
-require('sinon');
-require('sinon-mongoose');
+require('@lykmapipo/mongoose-test-helpers');
